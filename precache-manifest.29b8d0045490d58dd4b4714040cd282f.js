cfa481a54f3c911f8167af409a243925e0a22eea
eshaan5 <eshaanbagga@gmail.com>
2022-06-05 01:31:12 +0530

    Updates

---
+++ precache-manifest.29b8d0045490d58dd4b4714040cd282f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6fe278906a3b31098f3",
+    "url": "/copymatic-clone/static/css/main.9ec7758e.chunk.css"
+  },
+  {
+    "revision": "f6fe278906a3b31098f3",
+    "url": "/copymatic-clone/static/js/main.f6fe2789.chunk.js"
+  },
+  {
+    "revision": "4ab8efe26b871aabf1e3",
+    "url": "/copymatic-clone/static/css/1.c14c75ff.chunk.css"
+  },
+  {
+    "revision": "4ab8efe26b871aabf1e3",
+    "url": "/copymatic-clone/static/js/1.4ab8efe2.chunk.js"
+  },
+  {
+    "revision": "c2b74906860e038179aa",
+    "url": "/copymatic-clone/static/js/runtime~main.c2b74906.js"
+  },
+  {
+    "revision": "9564ceb2de78860e9c4dbb23cdbbf3e1",
+    "url": "/copymatic-clone/static/media/logo.9564ceb2.png"
+  },
+  {
+    "revision": "4885e6366d5642087089674fc9ead8bf",
+    "url": "/copymatic-clone/index.html"
+  }
+];
